0f4fa1a44d0bb41754b1ed2d59a27cbc3c3ca3a9
Abradave <109905261+Abradave@users.noreply.github.com>
2024-03-16 20:40:34 +0100

    first commit

---
+++ app/Http/Controllers/PublicTransportController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePublicTransportRequest;
+use App\Http\Requests\UpdatePublicTransportRequest;
+use App\Models\PublicTransport;
+
+class PublicTransportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePublicTransportRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PublicTransport $publicTransport)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PublicTransport $publicTransport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePublicTransportRequest $request, PublicTransport $publicTransport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PublicTransport $publicTransport)
+    {
+        //
+    }
+}
