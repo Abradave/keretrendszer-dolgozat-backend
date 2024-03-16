698432ab1105d7b1357e6cfeb5d1803c3c473967
Abradave <109905261+Abradave@users.noreply.github.com>
2024-03-16 21:57:54 +0100

    GetData

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\TransportControl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource("/public_transports", null);
+Route::apiResource("/public_transports", TransportControl::class);
