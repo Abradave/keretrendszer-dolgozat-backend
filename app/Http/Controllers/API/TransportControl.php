02a7538a9e96d09d1a0c4dc27f15349af14162c5
Abradave <109905261+Abradave@users.noreply.github.com>
2024-03-17 16:32:48 +0100

    Delete

--- app/Http/Controllers/API/TransportControl.php
+++ app/Http/Controllers/API/TransportControl.php
@@ -59,6 +59,11 @@ class TransportControl extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $transport = PublicTransport::find($id);
+        if (is_null($transport)) {
+            return response()->json(["message" => "Vehicle not found with this id: $id"], 404);
+        }
+        $transport->delete();
+        return response()->noContent();
     }
 }
